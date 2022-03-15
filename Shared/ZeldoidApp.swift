d2a627ea2d5bf8bdacd060ea667e59d5eb1fe231
Deirdre Saoirse Moen <deirdre@deirdre.net>
2022-03-15 15:05:09 -0700

    Initial Commit

---
+++ Shared/ZeldoidApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeldoidApp.swift
+//  Shared
+//
+//  Created by Deirdre Saoirse Moen on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ZeldoidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
